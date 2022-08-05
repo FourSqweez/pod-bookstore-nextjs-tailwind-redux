9d916f9e1c0f43d4b1a7659c2d49dfe060359cad
FourSqweez <four7518@gmail.com>
2022-08-05 16:41:28 +0700

    Edit : Edit css class to new styled

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,33 +1,30 @@
 import React from 'react'
-import imgTest from '../../public/assets/images/test.jpg'
 
 const ProductItem = () => {
 	return (
-		<div className="flex w-full sm:w-48 h-56 sm:h-96 flex-col items-center justify-center rounded-xl border border-red-500">
-			<div className="w-full h-20 sm:h-40 flex flex-col justify-self-center items-center border border-yellow-600">
-				<img
-					className="border border-green-400 w-full h-20 sm:h-40"
-					src="test.jpg"
-					alt=""
-				/>
+		<div className="flex hover:border-2 w-full sm:w-48 h-64 sm:h-96 flex-col items-center justify-center overflow-hidden rounded-xl hover:opacity-100 hover:shadow-slate-200 hover:shadow-lg transition duration-200 ease-ou">
+			<div className="w-full h-full flex flex-col justify-self-center items-center ">
+				<img className="w-full h-full sm:h-40" src="test.jpg" alt="" />
 			</div>
 			<div className="h-10 sm:h-1 my-2 sm:mb-0 text-center">
 				<h3 className="text-xs sm:text-xl sm:mt-2">bookname</h3>
 				<h2 className="text-xs sm:text-lg ">price: 200 bath</h2>
 			</div>
 
-			<div className="flex h-30 p-3 sm:mt-20 sm:h-20 w-full justify-evenly items-center border-2">
-				<button className="border-2  border-blue-400 w-10 h-10 rounded-l-lg">
+			<div className="flex h-14 p-3 sm:h-30 sm:mt-20 sm:h-20 w-full justify-evenly items-center ">
+				<button className="w-10 h-full rounded-l-lg">
 					<p className="text-xl">-</p>
 				</button>
-				<p className="text-xl">1</p>
-				<button className="border-2 flex justify-center items-center text-center border-blue-400 w-10 h-10 rounded-r-lg">
+				<p className="text-xl p-0.5 sm:p-3 mx-1 h-full border-b-2  text-center flex items-center">
+					1
+				</p>
+				<button className=" flex justify-center items-center text-center  w-10 h-full rounded-r-lg">
 					<p className="text-xl">+</p>
 				</button>
 			</div>
 
-			<div className="">
-				<button className="p-1 sm:p-3 rounded-xl bg-[#FFB86C]">
+			<div className="w-full">
+				<button className="p-0.5 sm:p-3 rounded-md w-full bg-[#FFB86C]">
 					<p className="text-[11px] sm:text-lg text-[#212b38]">
 						Add to cart
 					</p>
