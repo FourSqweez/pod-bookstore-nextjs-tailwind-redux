1cdbfb75981bb58af6179c3c38f298e8c6b0b0cb
FourSqweez <four7518@gmail.com>
2022-08-05 16:40:44 +0700

    Add : Edit css class

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,8 +3,8 @@ import ProductItem from './ProductItem'
 
 const Products = () => {
 	return (
-		<div className="max-w-7xl">
-			<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4 lg:gap-2 place-items-center">
+		<div className="m-w-7xl">
+			<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4 sm:gap-8 place-items-center">
 				<ProductItem />
 				<ProductItem />
 				<ProductItem />
