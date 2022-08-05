b786e3f45c5118efd9f66cab202cb070fa03de61
FourSqweez <four7518@gmail.com>
2022-08-05 08:22:12 +0700

    Add : Created Search components

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Search = () => {
+	return (
+		<div className='mb-10'>
+			<div className="flex w-full sm:w-96 lg:w-screen max-w-3xl mx-auto items-center">
+				<label htmlFor="" className="w-full">
+					<input type="text" className="w-full h-10 rounded-lg" />
+				</label>
+				<div className='ml-3'>ไอคอน</div>
+			</div>
+		</div>
+	)
+}
+
+export default Search
