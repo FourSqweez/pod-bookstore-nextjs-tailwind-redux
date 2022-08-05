6ea4df9553df8e3114755aaf24bb8beadef2ac2d
FourSqweez <four7518@gmail.com>
2022-08-05 17:52:12 +0700

    Add : Update style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Search from './Search'
+import { MdShoppingCart as CardIcon } from 'react-icons/md'
 
 const Navbar = () => {
 	return (
-		<div className="flex w-full justify-center">
-			<div className="w-full">
-				<Search />
-			</div>
-
-			<div className="">
-				<div>Card</div>
-				<div>Icon</div>
+		<div className="flex w-full">
+			<Search />
+			<div className="relative clicked h-full mr-2 sm:mr-10 flex w-16 cursor-pointer">
+				<span className="absolute bottom-1 right-2  h-6 w-6 bg-[#9dcaf8] text-center rounded-full text-black font-bold">
+					{/* {items.length} */} 4
+				</span>
+				<CardIcon className="w-28 h-12" />
 			</div>
 		</div>
 	)
