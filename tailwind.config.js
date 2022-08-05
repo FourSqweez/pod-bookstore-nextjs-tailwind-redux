46537ef8e7c199883ad66ae41bc304cba13865be
FourSqweez <four7518@gmail.com>
2022-08-06 02:41:17 +0700

    Add : Custom breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
 		'./src/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
+		screens: {
+			'xs': '430px',
+			'sm': '640px',
+			'md': '768px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
 		extend: {},
 	},
 	plugins: [],
