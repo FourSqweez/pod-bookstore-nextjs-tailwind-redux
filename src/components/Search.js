ea1840fa242c3a20916481607d9cee95ba133465
FourSqweez <four7518@gmail.com>
2022-08-05 16:42:12 +0700

    Edit : Add css class to styled

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 const Search = () => {
 	return (
-		<div className='mb-10'>
-			<div className="flex w-full sm:w-96 lg:w-screen max-w-3xl mx-auto items-center">
-				<label htmlFor="" className="w-full">
-					<input type="text" className="w-full h-10 rounded-lg" />
-				</label>
-				<div className='ml-3'>ไอคอน</div>
+		<div className="mb-10 mr-3">
+			<div className="flex w-full sm:w-96 lg:w-screen max-w-3xl mx-auto  items-center">
+				<div className="flex sm:w-full items-center border-2 rounded-full py-1 md:shadow-sm">
+					<FaSearch className="w-8 md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 mx-2" />
+					<input
+						// value={searchInput}
+						// onChange={(e) => setSearchInput(e.target.value)}
+						type="text"
+						// placeholder={placeholder || 'Start your search'}
+						className="w-full self-center pb-1 pr-5 bg-transparent outline-none text-2xl text-gray-300 placeholder-gray-400"
+					/>
+				</div>
 			</div>
 		</div>
 	)
