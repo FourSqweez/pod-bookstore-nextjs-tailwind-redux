57fd360674d9c807e73cde64c357b69ac3714097
FourSqweez <four7518@gmail.com>
2022-08-06 14:59:36 +0700

    Edit : update some styled

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,14 +5,14 @@ const Search = () => {
 	return (
 		<div className="mb-10 w-full flex justify-center">
 			<div className="flex w-full lg:w-screen max-w-3xl items-center">
-				<div className="flex w-full items-center border-2 rounded-full py-1 md:shadow-sm">
-					<FaSearch className="w-8 md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 mx-2" />
+				<div className="flex w-full items-center border-2 border-black rounded-xl py-1 md:shadow-sm">
+					<FaSearch className="w-8 md:inline-flex h-8 text-gray-400 rounded-full p-2 mx-2" />
 					<input
 						// value={searchInput}
 						// onChange={(e) => setSearchInput(e.target.value)}
 						type="text"
 						// placeholder={placeholder || 'Start your search'}
-						className="w-full self-center pb-1 pr-5 bg-transparent outline-none text-2xl text-gray-300 placeholder-gray-400"
+						className="w-full self-center pb-1 pr-5 bg-transparent outline-none text-2xl text-black placeholder-gray-400"
 					/>
 				</div>
 			</div>
