8b27c1a89f43fc1551f2f46664e1b239249f4bf7
FourSqweez <four7518@gmail.com>
2022-08-06 21:06:24 +0700

    Update : Add lg breakpoint

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,9 +4,9 @@ import ProductItem from './ProductItem'
 const Products = ({ books }) => {
 	return (
 		<div className="m-w-7xl">
-			<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4 sm:gap-8 place-items-center">
+			<div className="grid grid-cols-2 sm:grid-cols-3  lg:grid-cols-4 xl:grid-cols-6 gap-4 sm:gap-8 place-items-center">
 				{books &&
-					books.map((book) => <ProductItem book={book} key={book} />)}
+					books.map((book) => <ProductItem book={book} key={book.id} />)}
 			</div>
 		</div>
 	)
