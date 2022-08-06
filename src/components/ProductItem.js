410896ef8c28c032f52c4b771ccf2cbd254961a1
FourSqweez <four7518@gmail.com>
2022-08-06 22:40:49 +0700

    Edit : Rename from book to product

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 
-const ProductItem = ({ book }) => {
+const ProductItem = ({ product }) => {
 	return (
 		<div className="w-40 h-full border-2 hover:border-[#F692B2] bg-[#ffd580] sm:w-48 sm:h-full overflow-hidden rounded-xl hover:opacity-100 hover:shadow-slate-200 hover:shadow-lg hover:scale-110 duration-200 ease-ou">
 			<div className="h-52 w-full sm:h-60 sm:w-full relative  overflow-hidden rounded-t-xl">
 				<Image
-					src={book.images.jpeg}
+					src={product.images.jpeg}
 					layout="fill"
 					alt=""
 					objectFit="inherit"
@@ -14,10 +14,15 @@ const ProductItem = ({ book }) => {
 				/>
 			</div>
 			<div className="flex px-2 flex-col justify-end sm:mb-0 sm:w-full ">
-				<h4 className="text-xs h-full sm:text-sm title" title={book.title}>
-					{book.title}
+				<h4
+					className="text-xs h-full sm:text-sm title"
+					title={product.title}
+				>
+					{product.title}
 				</h4>
-				<h4 className="text-xs sm:text-sm h-full">price: {book.price}</h4>
+				<h4 className="text-xs sm:text-sm h-full">
+					price: {product.price}
+				</h4>
 			</div>
 
 			<div className="flex w-full justify-center gap-4 sm:my-1 items-center">
--- src/components/ProductsFeed.js
+++ src/components/ProductsFeed.js
@@ -6,7 +6,9 @@ const ProductsFeed = ({ books }) => {
 		<div className="m-w-7xl">
 			<div className="grid grid-cols-2 sm:grid-cols-3  lg:grid-cols-4 xl:grid-cols-6 gap-4 sm:gap-8 place-items-center">
 				{books &&
-					books.map((book) => <ProductItem book={book} key={book.id} />)}
+					books.map((product) => (
+						<ProductItem product={product} key={product.id} />
+					))}
 			</div>
 		</div>
 	)
