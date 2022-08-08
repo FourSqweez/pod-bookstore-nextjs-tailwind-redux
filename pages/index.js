95ef30e616c17d1be75f2757668f1e52a2a5db8a
FourSqweez <four7518@gmail.com>
2022-08-09 02:42:25 +0700

    Update : display product from search

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,57 @@
 import Head from 'next/head'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductsFeed from '../src/components/ProductsFeed'
 import { getBooksAPI, getPromotionsAPI } from './api/booksApi'
 import { useDispatch } from 'react-redux'
 import { addPromotions } from '../src/app/slices/promotionSlice'
-import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
 
 export default function Home({ books, promotions }) {
+	const router = useRouter()
+	const { searchInput, setSearchInput } = router.query
 	const dispatch = useDispatch()
+	const [newBook, setNewBook] = useState([])
+
+	const a = 'Harry is find'
+	const b = a.search('Harry is find')
+
+	console.log('Test : ', b)
+	console.log('Book quantity : ', newBook.length)
+
+	useEffect(() => {
+		//const bookTitle = books.map((b) => b.title)
+		//console.log('Book Title : ', bookTitle)
+		//const book = books.map((book) => book)
+		// const bookFromSearch = bookTitle.filter((str) =>
+		// 	str.toLowerCase().match(searchInput)
+		// )
+		// console.log('bookFromSearch : ', bookFromSearch)
+
+		if (searchInput) {
+			const newBooks = books.filter((book) =>
+				book.title.toLowerCase().match(searchInput)
+			)
+			setNewBook(newBooks)
+		}
+	}, [searchInput])
+
+	console.log('New book :', newBook)
+
+	console.log('Search : ', searchInput)
 	useEffect(() => {
 		dispatch(addPromotions(promotions))
 	}, [1])
+
+	// const filterDataToRender = (e) => {
+	// 	if (newBook.length === 0 && searchInput.isEmpty) {
+	// 		return <ProductsFeed books={books} />
+	// 	} else if (newBook.length !== 0 && e.key === 'Enter') {
+	// 		return <ProductsFeed books={newBook} />
+	// 	} else if (newBook.length === 0 && e.key === 'Enter') {
+	// 		return `Not have a books that match with '${searchInput}'`
+	// 	}
+	// 	return <ProductsFeed books={books} />
+	// }
 
 	return (
 		<div className="mx-1 sm:mx-10">
@@ -20,7 +61,11 @@ export default function Home({ books, promotions }) {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className="mt-5 flex flex-col justify-center items-center">
-				<ProductsFeed books={books} />
+				{newBook.length === 0 ? (
+					<ProductsFeed books={books} />
+				) : (
+					<ProductsFeed books={newBook} />
+				)}
 			</main>
 		</div>
 	)
