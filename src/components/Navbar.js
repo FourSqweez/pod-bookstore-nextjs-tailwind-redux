65dba705a5091aba1b61121a660dd6c1917222bd
FourSqweez <four7518@gmail.com>
2022-08-09 02:44:32 +0700

    Update : refactor code and update some styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,11 @@
 import React from 'react'
-import Search from './Search'
 import { MdShoppingCart as CardIcon } from 'react-icons/md'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
-
-const MyButtonHome = React.forwardRef(({ onClick, href }, ref) => {
-	return (
-		<a href={href} onClick={onClick} ref={ref}>
-			<div>
-				<h3 className="text-2xl">Little Brown</h3>
-				<h3 className="text-2xl">Book Shop.</h3>
-			</div>
-		</a>
-	)
-})
+import SearchInput from './SearchInput'
 
 const CartButton = React.forwardRef(({ onClick, href }, ref) => {
-	return (
-		<a href={href} onClick={onClick} ref={ref} className="w-full">
-			<CardIcon className="w-full h-full" />
-		</a>
-	)
-})
-
-const Navbar = () => {
-	const router = useRouter()
-	const cart = useSelector((state) => state.cart)
-	console.log('Router :', router.asPath)
 	const quantity = useSelector((state) => state.cart.map((q) => q.quantity))
 
 	const totalAmounts = quantity.reduce(
@@ -37,26 +15,39 @@ const Navbar = () => {
 	console.log(`Total amount : ${totalAmounts}`)
 
 	console.log('quantity ', quantity)
+	return (
+		<div
+			onClick={onClick}
+			className="relative clicked h-full mr-2 sm:mr-10 flex w-16 cursor-pointer"
+		>
+			<span className="absolute bottom-1 right-1 h-6 w-6 bg-[#9dcaf8] text-center rounded-full text-black font-bold">
+				{totalAmounts === 0 ? 0 : totalAmounts}
+			</span>
+			<a href={href} ref={ref} className="w-16">
+				<CardIcon className="w-full h-full" />
+			</a>
+		</div>
+	)
+})
+
+const Navbar = () => {
+	const router = useRouter()
+	const cart = useSelector((state) => state.cart)
+	console.log('Router :', router.asPath)
 
 	return (
-		<div className="flex w-full mt-5">
+		<div className="flex h-full w-full mt-5 justify-between items-center">
 			<div className="hidden sm:flex pl-10 mb-3 w-48 whitespace-nowrap cursor-pointer">
 				<Link href="/" passHref>
-					<MyButtonHome />
+					<h3 className="text-2xl">
+						Little Brown <br /> Book Shop.
+					</h3>
 				</Link>
 			</div>
-			<div className="w-full mx-3">
-				{router && router.asPath == '/checkout' ? '' : <Search />}
-			</div>
-
-			<div className="relative clicked h-full mr-2 sm:mr-10 flex w-16 cursor-pointer">
-				<span className="absolute bottom-1 right-0  h-6 w-6 bg-[#9dcaf8] text-center rounded-full text-black font-bold">
-					{totalAmounts === 0 ? 0 : totalAmounts}
-				</span>
-				<Link href="/checkout" passHref>
-					<CartButton />
-				</Link>
-			</div>
+			{router && router.asPath == '/checkout' ? '' : <SearchInput />}
+			<Link href="/checkout" passHref>
+				<CartButton />
+			</Link>
 		</div>
 	)
 }
