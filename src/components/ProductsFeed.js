4b383081dcfc3037172e154c4a8942408d06cb69
FourSqweez <four7518@gmail.com>
2022-08-10 21:27:49 +0700

    Update : push quantity to books object

--- src/components/ProductsFeed.js
+++ src/components/ProductsFeed.js
@@ -2,13 +2,26 @@ import React from 'react'
 import ProductItem from './ProductItem'
 
 const ProductsFeed = ({ books }) => {
+	const booksAndQuantity = books.map((book) => {
+		return { quantity: 0, ...book }
+	})
+
+	const handleProps = () => {
+		return booksAndQuantity.map((product) => {
+			return (
+				<ProductItem
+					booksAndQuantity={booksAndQuantity}
+					product={product}
+					key={product.id}
+				/>
+			)
+		})
+	}
+
 	return (
 		<div className="m-w-7xl">
 			<div className="grid grid-cols-2 sm:grid-cols-3  lg:grid-cols-4 xl:grid-cols-6 gap-4 sm:gap-8 place-items-center">
-				{books &&
-					books.map((product) => (
-						<ProductItem product={product} key={product.id} />
-					))}
+				{books && handleProps()}
 			</div>
 		</div>
 	)
