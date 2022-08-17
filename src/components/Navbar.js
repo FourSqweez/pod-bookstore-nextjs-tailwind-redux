173b284620eb172b41abbb0a2919f053372abed2
FourSqweez <four7518@gmail.com>
2022-08-17 16:35:44 +0700

    Add : Condition to hidden navbar when modalIsOpen == true

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { MdShoppingCart as CardIcon } from 'react-icons/md'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
 import SearchInput from './SearchInput'
+import { ModalContext } from '../context/context'
 
 const CartButton = React.forwardRef(({ onClick, href }, ref) => {
 	const quantity = useSelector((state) => state.cart.map((q) => q.quantity))
@@ -30,26 +31,33 @@ CartButton.displayName = 'CartButton'
 
 const Navbar = () => {
 	const router = useRouter()
+	const { isModalOpen } = useContext(ModalContext)
 	return (
-		<div className="flex h-16 w-full mt-5 mb-7 sm:justify-between items-center justify-end">
-			<div className="hidden sm:flex ml-10 whitespace-nowrap cursor-pointer">
-				<Link href="/" passHref>
-					<h3 className="text-2xl">
-						Little Brown <br /> Book Shop.
-					</h3>
-				</Link>
-			</div>
-			{router && router.asPath == '/checkout' ? (
+		<>
+			{isModalOpen ? (
 				''
 			) : (
-				<>
-					<SearchInput />
-					<Link href="/checkout" passHref>
-						<CartButton />
-					</Link>
-				</>
+				<div className="flex h-16 w-full mt-5 mb-7 sm:justify-between items-center justify-end">
+					<div className="hidden sm:flex ml-10 whitespace-nowrap cursor-pointer">
+						<Link href="/" passHref>
+							<h3 className="text-2xl">
+								Little Brown <br /> Book Shop.
+							</h3>
+						</Link>
+					</div>
+					{router && router.asPath == '/checkout' ? (
+						''
+					) : (
+						<>
+							<SearchInput />
+							<Link href="/checkout" passHref>
+								<CartButton />
+							</Link>
+						</>
+					)}
+				</div>
 			)}
-		</div>
+		</>
 	)
 }
 
