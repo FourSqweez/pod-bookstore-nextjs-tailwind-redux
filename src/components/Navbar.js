5aff34bf12d46874f5f6eb2b8c4b23a8060c7072
FourSqweez <four7518@gmail.com>
2022-08-17 23:37:56 +0700

    Add : new condition

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
 			{isModalOpen ? (
 				''
 			) : (
-				<div className="flex h-16 w-full mt-5 mb-7 sm:justify-between items-center justify-end">
+				<div className="flex h-16 w-full mt-5 mb-7 sm:justify-between items-center">
 					<div className="hidden sm:flex ml-10 whitespace-nowrap cursor-pointer">
 						<Link href="/" passHref>
 							<h3 className="text-2xl">
@@ -46,7 +46,13 @@ const Navbar = () => {
 						</Link>
 					</div>
 					{router && router.asPath == '/checkout' ? (
-						''
+						<div className="sm:hidden ml-4 whitespace-nowrap cursor-pointer">
+							<Link href="/" passHref>
+								<h3 className="text-2xl">
+									Little Brown <br /> Book Shop.
+								</h3>
+							</Link>
+						</div>
 					) : (
 						<>
 							<SearchInput />
