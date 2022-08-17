2f8bdff959116875a41ab2ac37490a8e9dfa71de
FourSqweez <four7518@gmail.com>
2022-08-17 16:34:08 +0700

    Add : Created modal context provider to wrap all child component

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,18 @@ import Navbar from '../src/components/Navbar'
 import '../styles/globals.css'
 import store from '../src/app/store'
 import { Provider } from 'react-redux'
+import React, { useState } from 'react'
+import { ModalContext } from '../src/context/context'
 
 function MyApp({ Component, pageProps }) {
+	const [isModalOpen, setIsModalOpen] = useState(false)
+
 	return (
 		<Provider store={store}>
-			<Navbar />
-			<Component {...pageProps} />
+			<ModalContext.Provider value={{ isModalOpen, setIsModalOpen }}>
+				<Navbar />
+				<Component {...pageProps} />
+			</ModalContext.Provider>
 		</Provider>
 	)
 }
