b35d36d4717127e4a64f7ca990aafa8227e8c32d
FourSqweez <four7518@gmail.com>
2022-08-17 21:44:22 +0700

    Add : Loading bar on the top

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,19 @@ import store from '../src/app/store'
 import { Provider } from 'react-redux'
 import React, { useState } from 'react'
 import { ModalContext } from '../src/context/context'
+import Router from 'next/router'
+import ProgressBar from '@badrap/bar-of-progress'
+
+const progress = new ProgressBar({
+	size: 4,
+	color: '#F692B2',
+	className: 'z-50',
+	delay: 100,
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 function MyApp({ Component, pageProps }) {
 	const [isModalOpen, setIsModalOpen] = useState(false)
